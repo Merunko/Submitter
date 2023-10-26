78d390e307981359f8b4ddcc6a084c256ae6a286
Merunko <141312536+Merunko@users.noreply.github.com>
2023-10-27 05:11:53 +0800

    Add files via upload

--- src/main/java/me/merunko/holocraft/Listener/InventoryInteractListener.java
+++ src/main/java/me/merunko/holocraft/Listener/InventoryInteractListener.java
@@ -1,23 +1,27 @@
 package me.merunko.holocraft.Listener;
 
 import me.merunko.holocraft.Holder.InventoryGuiHolder;
+import net.Indyuce.mmoitems.MMOItems;
+import net.Zrips.CMILib.Colors.CMIChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class InventoryInteractListener implements Listener {
@@ -39,6 +43,8 @@ public class InventoryInteractListener implements Listener {
 
         if (clickedInventory != null && clickedInventory.getHolder() instanceof InventoryGuiHolder) {
 
+            updateDynamicWorthValue(clickedInventory);
+
             if (!((clickedSlot >= 10 && clickedSlot <= 16) || (clickedSlot >= 19 && clickedSlot <= 25) || (clickedSlot >= 28 && clickedSlot <= 34) || (clickedSlot >= 37 && clickedSlot <= 43))) {
                 e.setCancelled(true);
             }
@@ -46,7 +52,9 @@ public class InventoryInteractListener implements Listener {
             if (clickedSlot == 50) {
                 e.setCancelled(true);
                 Player player = (Player) e.getWhoClicked();
+                returnItems(e.getClickedInventory(), player.getInventory());
                 player.closeInventory();
+                player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1.0F, 1.0F);
             }
 
             if (clickedSlot == 48) {
@@ -58,26 +66,36 @@ public class InventoryInteractListener implements Listener {
                 StringBuilder submittedItems = new StringBuilder();
 
                 for (int slot = 10; slot <= 16; slot++) {
-                    totalPoints += calculatePoints(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    if (inventory != null) {
+                        totalPoints += calculatePoint(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    }
                 }
                 for (int slot = 19; slot <= 25; slot++) {
-                    totalPoints += calculatePoints(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    if (inventory != null) {
+                        totalPoints += calculatePoint(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    }
                 }
                 for (int slot = 28; slot <= 34; slot++) {
-                    totalPoints += calculatePoints(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    if (inventory != null) {
+                        totalPoints += calculatePoint(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    }
                 }
                 for (int slot = 37; slot <= 43; slot++) {
-                    totalPoints += calculatePoints(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    if (inventory != null) {
+                        totalPoints += calculatePoint(inventory.getItem(slot), player.getInventory(), submittedItems);
+                    }
                 }
 
                 String playerName = player.getName();
                 logSubmission(playerName, submittedItems.toString(), totalPoints);
 
                 if (totalPoints > 0) {
-                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "is admin addbonus " + playerName + " worth " + totalPoints);
+                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "is admin addbonus " + player.getName() + " worth " + totalPoints);
                     player.sendMessage(ChatColor.GREEN + "Your island gained " + ChatColor.GOLD + totalPoints + ChatColor.GREEN + " worth value.");
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_CELEBRATE, 1.0F, 1.0F);
                 } else {
                     player.sendMessage(ChatColor.RED + "All the items submitted have no value.");
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1.0F, 1.0F);
                 }
 
                 player.closeInventory();
@@ -85,19 +103,167 @@ public class InventoryInteractListener implements Listener {
         }
     }
 
-    private int calculatePoints(ItemStack item, Inventory playerInventory, StringBuilder submittedItems) {
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        Inventory closedInventory = e.getInventory();
+        Player player = (Player) e.getPlayer();
+        if (!closedInventory.isEmpty() && closedInventory.getHolder() instanceof InventoryGuiHolder && e.getReason() != InventoryCloseEvent.Reason.PLUGIN ) {
+            returnItems(closedInventory, player.getInventory());
+        }
+    }
+
+    public void returnItems(Inventory inv, Inventory playerInv) {
+        for (int slot = 10; slot <= 16; slot++) {
+            ItemStack item = inv.getItem(slot);
+            if(item != null) {
+                playerInv.addItem(item);
+            }
+        }
+        for (int slot = 19; slot <= 25; slot++) {
+            ItemStack item = inv.getItem(slot);
+            if(item != null) {
+                playerInv.addItem(item);
+            }
+        }
+        for (int slot = 28; slot <= 34; slot++) {
+            ItemStack item = inv.getItem(slot);
+            if(item != null) {
+                playerInv.addItem(item);
+            }
+        }
+        for (int slot = 37; slot <= 43; slot++) {
+            ItemStack item = inv.getItem(slot);
+            if(item != null) {
+                playerInv.addItem(item);
+            }
+        }
+    }
+
+    private boolean typeMatches(String mmoItemType, String typeName) {
+        return mmoItemType.equals(typeName);
+    }
+
+    private boolean itemIdMatches(String mmoItemId, String itemId) {
+        return mmoItemId.equals(itemId);
+    }
+
+    private int calculatePoint(ItemStack item, Inventory playerInventory, StringBuilder submittedItems) {
+        String mmoItemType = MMOItems.getTypeName(item);
+        String mmoItemId = MMOItems.getID(item);
+
         if (item != null) {
             Material itemType = item.getType();
-            if (config.isConfigurationSection("items") && config.contains("items." + itemType)) {
-                int points = config.getInt("items." + itemType);
-                playerInventory.removeItem(item);
-                submittedItems.append(item.getAmount()).append(" ").append(itemType).append(", ");
-                return points * item.getAmount();
+            if (mmoItemType != null && mmoItemId != null) {
+                if (config.isConfigurationSection("mmoitems")) {
+                    boolean foundMatch = false;
+
+                    for (String mmoType : Objects.requireNonNull(config.getConfigurationSection("mmoitems")).getKeys(false)) {
+                        if (config.isList("mmoitems." + mmoType)) {
+                            List<String> itemValuePairs = config.getStringList("mmoitems." + mmoType);
+                            for (String itemValuePair : itemValuePairs) {
+                                String[] parts = itemValuePair.split(":");
+                                if (parts.length == 2) {
+                                    String itemId = parts[0].trim();
+                                    int value = Integer.parseInt(parts[1].trim());
+
+                                    if (typeMatches(mmoItemType, mmoType) && itemIdMatches(mmoItemId, itemId)) {
+                                        submittedItems.append(item.getAmount()).append(" ").append(itemId).append(", ");
+                                        return value * item.getAmount();
+                                    }
+                                }
+                            }
+                        }
+                    } if (!foundMatch) {
+                        playerInventory.addItem(item);
+                    }
+                }
+
             } else {
-                playerInventory.addItem(item);
+                if (config.isConfigurationSection("minecraft_items") && config.contains("minecraft_items." + itemType)) {
+                    int points = config.getInt("minecraft_items." + itemType);
+                    submittedItems.append(item.getAmount()).append(" ").append(itemType).append(", ");
+                    return points * item.getAmount();
+                } else {
+                    playerInventory.addItem(item);
+                }
             }
         }
         return 0;
+    }
+
+    private int dynamicWorthValue(ItemStack item) {
+        if (item == null) {
+            return 0;
+        }
+
+        String mmoItemType = MMOItems.getTypeName(item);
+        String mmoItemId = MMOItems.getID(item);
+
+        int value = 0;
+
+        Material itemType = item.getType();
+        if (mmoItemType != null && mmoItemId != null) {
+            if (config.isConfigurationSection("mmoitems")) {
+
+                for (String mmoType : Objects.requireNonNull(config.getConfigurationSection("mmoitems")).getKeys(false)) {
+                    if (config.isList("mmoitems." + mmoType)) {
+                        List<String> itemValuePairs = config.getStringList("mmoitems." + mmoType);
+                        for (String itemValuePair : itemValuePairs) {
+                            String[] parts = itemValuePair.split(":");
+                            if (parts.length == 2) {
+                                String itemId = parts[0].trim();
+                                int valueMMO = Integer.parseInt(parts[1].trim());
+
+                                if (typeMatches(mmoItemType, mmoType) && itemIdMatches(mmoItemId, itemId)) {
+                                    value += valueMMO * item.getAmount();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        } else {
+            if (config.isConfigurationSection("minecraft_items") && config.contains("minecraft_items." + itemType)) {
+                int valueVanilla = config.getInt("minecraft_items." + itemType);
+                value += valueVanilla * item.getAmount();
+            }
+        }
+        return value;
+    }
+
+    private void updateDynamicWorthValue(Inventory inventory) {
+        int totalWorth = 0;
+        for (int slot = 10; slot <= 16; slot++) {
+            ItemStack item = inventory.getItem(slot);
+            totalWorth += dynamicWorthValue(item);
+        }
+        for (int slot = 19; slot <= 25; slot++) {
+            ItemStack item = inventory.getItem(slot);
+            totalWorth += dynamicWorthValue(item);
+        }
+        for (int slot = 28; slot <= 34; slot++) {
+            ItemStack item = inventory.getItem(slot);
+            totalWorth += dynamicWorthValue(item);
+        }
+        for (int slot = 37; slot <= 43; slot++) {
+            ItemStack item = inventory.getItem(slot);
+            totalWorth += dynamicWorthValue(item);
+        }
+
+        ItemStack totalWorthSign = new ItemStack(Material.ACACIA_HANGING_SIGN);
+        ItemMeta totalWorthMeta = totalWorthSign.getItemMeta();
+        if (totalWorthMeta != null) {
+            String name3 = CMIChatColor.translate("{#FB5848}Worth: {#FB8F00}" + totalWorth);
+            totalWorthMeta.setDisplayName(name3);
+
+            List<String> lore = new ArrayList<>();
+            lore.add(CMIChatColor.translate("{#B9FBE1}Click this if worth value not updated."));
+            totalWorthMeta.setLore(lore);
+
+            totalWorthSign.setItemMeta(totalWorthMeta);
+        }
+        inventory.setItem(49, totalWorthSign);
     }
 
     private void logSubmission(String playerName, String items, int totalPoints) {
